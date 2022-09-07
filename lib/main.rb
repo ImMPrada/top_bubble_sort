b5e93ba5426aacd3baff5f795e0a9ed320674b96
immprada <im.mprada@gmail.com>
2022-09-07 17:06:39 -0500

    upgrade by discussion

--- lib/main.rb
+++ lib/main.rb
@@ -1,14 +1,11 @@
 def bubble_sort(numbers)
   sorted = true
 
-  (numbers.length - 1).times do |i|
-    sub_arr = [numbers[i], numbers[i + 1]]
+  (1...numbers.length).each do |i|
+    next if numbers[i - 1] <= numbers[i]
 
-    next if sub_arr[0] <= sub_arr[1]
-
-    numbers = numbers[0, i] + sub_arr.sort! + numbers[i + 2...]
+    numbers = numbers[0, i - 1] + [numbers[i], numbers[i - 1]] + numbers[i + 1...]
     sorted = false
-    break
   end
 
   return bubble_sort(numbers) unless sorted
